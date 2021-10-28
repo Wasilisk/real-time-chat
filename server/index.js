1a93b5f969cc45995285c204cb2b83e84d34e421
Vasyl <vasylpetryna2002@gmail.com>
2021-10-28 17:47:55 +0300

    Added basic functionality and simple chat design

--- server/index.js
+++ server/index.js
@@ -14,10 +14,19 @@ const io = new Server(server, {
     }
 })
 
-io.on("connection", (socket)=> {
-    console.log(socket.id);
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
 
-    socket.on("disconnect", ()=> {
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room ${data}`)
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit('receive_message', data)
+    })
+
+    socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id)
     });
 })
